88b004dfc02fbc62e03818f7cc547b43faed330e
PuniCharana <punicharana@gmail.com>
2018-03-28 15:14:58 +0530

    first commit

---
+++ app/src/main/java/me/spotytube/themedemo/MainActivity.kt
@@ -0,0 +1,133 @@
+package me.spotytube.themedemo
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.internal.BottomNavigationItemView
+import android.support.design.internal.BottomNavigationMenuView
+import android.support.design.widget.BottomNavigationView
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_discover -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_search -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_artists -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_my_music -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        BottomNavigationViewHelper.removeShiftMode(navigation)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    internal object BottomNavigationViewHelper {
+        @SuppressLint("RestrictedApi")
+        fun removeShiftMode(view: BottomNavigationView) {
+            val menuView = view.getChildAt(0) as BottomNavigationMenuView
+            try {
+                val shiftingMode = menuView.javaClass.getDeclaredField("mShiftingMode")
+                shiftingMode.isAccessible = true
+                shiftingMode.setBoolean(menuView, false)
+                shiftingMode.isAccessible = false
+                for (i in 0 until menuView.childCount) {
+                    val item = menuView.getChildAt(i) as BottomNavigationItemView
+                    item.setShiftingMode(false)
+                    // set once again checked value, so view will be updated
+                    item.setChecked(item.itemData.isChecked)
+                }
+            } catch (e: NoSuchFieldException) {
+                Log.e("ERROR NO SUCH FIELD", "Unable to get shift mode field")
+            } catch (e: IllegalAccessException) {
+                Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode")
+            }
+
+        }
+    }
+}
